1ce1112eab5ad54e50ec77923af827240df767cf
Sushanta <sushanta.panda77@gmail.com>
2021-05-09 21:15:44 +0530

    .

--- CS598 Practical Statistical Learning/Quiz/Quiz - 9.R
+++ CS598 Practical Statistical Learning/Quiz/Quiz - 9.R
@@ -6,16 +6,69 @@ testID = c(1:100, 1901:1960)
 spam.test=spam[testID, ]; 
 spam.train=spam[-testID, ];
 
+
+##------------------------------------------------ 
 ## Linear SVM  
+##------------------------------------------------
+#Training Error (Cost = 1)
 svmfit=svm(Y ~., kernel="linear", data=spam.train, cost=1)
 summary(svmfit)
 table(spam.train$Y, svmfit$fitted)
+179 + 112
 svmpred=predict(svmfit, newdata=spam.test)
 table(spam.test$Y, svmpred)
+12+2
 
-## Gaussian kernel SVM  
+#Training Error (Cost = 10)
+svmfit=svm(Y ~., kernel="linear", data=spam.train, cost=10)
+summary(svmfit)
+table(spam.train$Y, svmfit$fitted)
+178 + 110
+svmpred=predict(svmfit, newdata=spam.test)
+table(spam.test$Y, svmpred)
+11 + 3
+
+#Training Error (Cost = 50)
+svmfit=svm(Y ~., kernel="linear", data=spam.train, cost=50)
+summary(svmfit)
+table(spam.train$Y, svmfit$fitted)
+179 + 112
+svmpred=predict(svmfit, newdata=spam.test)
+table(spam.test$Y, svmpred)
+11 + 3
+
+
+
+##------------------------------------------------ 
+#Gaussian kernel SVM  
+##------------------------------------------------
+#Training Error (Cost = 1)
 svmfit=svm(Y ~., data=spam.train, cost=1)
 summary(svmfit)
 table(spam.train$Y, svmfit$fitted)
+147 + 85
 svmpred=predict(svmfit, newdata=spam.test)
 table(spam.test$Y, svmpred)
+10 + 4
+
+
+#Training Error (Cost = 10)
+svmfit=svm(Y ~., data=spam.train, cost=10)
+summary(svmfit)
+table(spam.train$Y, svmfit$fitted)
+99 + 47
+
+svmpred=predict(svmfit, newdata=spam.test)
+table(spam.test$Y, svmpred)
+11 + 4
+
+#Training Error (Cost = 50)
+svmfit=svm(Y ~., data=spam.train, cost=50)
+summary(svmfit)
+table(spam.train$Y, svmfit$fitted)
+63 + 27
+
+svmpred=predict(svmfit, newdata=spam.test)
+table(spam.test$Y, svmpred)
+14 + 3
+
